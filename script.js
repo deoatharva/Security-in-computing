0ca19e6666abfc05ec7d6960e59232cdb16a5524
deoatharva <152968808+deoatharva@users.noreply.github.com>
2025-01-29 22:00:19 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,114 @@
+console.log("welcome to spotify");
+//initailize the variables
+let songIndex = 0;
+let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSong = document.getElementById('masterSong'); 
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+let songs = [
+    {songName: "Intro",filePath: "0.mp3", coverPath: "1.jpg"},
+    {songName: "Chapter 1",filePath: "1.mp3", coverPath: "1.jpg"},
+    {songName: "Chapter 2",filePath: "2.mp3", coverPath: "1.jpg"},
+    {songName: "Chapter 3",filePath: "3.mp3", coverPath: "1.jpg"},
+    {songName: "Chapter 4",filePath: "4.mp3", coverPath: "1.jpg"},
+    {songName: "Chapter 5",filePath: "5.mp3", coverPath: "1.jpg"},
+    {songName: "Chapter 6",filePath: "6.mp3", coverPath: "1.jpg"},
+    {songName: "Chapter 7",filePath: "7.mp3", coverPath: "1.jpg"},
+    {songName: "Chapter 8",filePath: "8.mp3", coverPath: "1.jpg"},
+    {songName: "Chapter 9",filePath: "9.mp3", coverPath: "1.jpg"},
+    {songName: "Final Thoughts",filePath: "10.mp3", coverPath: "1.jpg"}
+]
+
+songItems.forEach((element, i)=>{
+    
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+})
+
+//handle play pause click
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-circle-pause');
+        masterPlay.classList.add('fa-circle-play');
+        gif.style.opacity = 0;
+    }
+})
+
+//lsiten to music
+audioElement.addEventListener('timeupdate',()=>{
+    
+    //update seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
+    
+    myProgressBar.value = progress;
+
+})
+
+myProgressBar.addEventListener('change',()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeALLPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-circle-pause');
+        element.classList.add('fa-circle-play');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{
+        makeALLPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-circle-play');
+        e.target.classList.add('fa-circle-pause');
+        audioElement.src = `${songIndex}.mp3`;
+        masterSong.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+    })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex >= 9){
+        songIndex = 0;
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = `${songIndex}.mp3`;
+    masterSong.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    gif.style.opacity = 1;
+    masterPlay.classList.remove('fa-circle-play');
+    masterPlay.classList.add('fa-circle-pause');
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex <= 0){
+        songIndex = 0;
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `${songIndex}.mp3`;
+    masterSong.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    gif.style.opacity = 1;
+    masterPlay.classList.remove('fa-circle-play');
+    masterPlay.classList.add('fa-circle-pause');
+})
